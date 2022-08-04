02d31606cd7eca30440044864087045e2ceba547
jaydeeay <jarroyavep@outlook.com>
2022-08-04 12:54:27 -0500

    UIInterface
    Se cambia el reset del jugador al UI_Manager por medio de un script UIInterface

---
+++ Assets/Scripts/UIInterface.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIInterface : MonoBehaviour
+{
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
