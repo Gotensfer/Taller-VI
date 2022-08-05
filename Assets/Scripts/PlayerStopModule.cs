35c66d74b8cb4a8d5c903e747213f44da010f718
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-08-05 01:56:24 -0500

    Casi lista primera fase

---
+++ Assets/Scripts/PlayerStopModule.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStopModule : MonoBehaviour
+{
+    [SerializeField] private byte bounces = 3, bouncesUsed = 0;
+    private Rigidbody2D rb;
+    private bool playerStopped = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();//getplayer()
+    }
+
+    public void StopBounce()
+    {
+        if (!playerStopped)
+        {
+            bouncesUsed++;
+            if (bouncesUsed >= bounces)
+            {
+                playerStopped = true;
+                StopPlayer();
+            }
+        }
+    }
+    
+    void StopPlayer()
+    {
+        
+    }
+}
