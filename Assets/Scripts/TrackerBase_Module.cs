147e5fb238bda45c798666ccf7a5838d816f8045
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-08-11 23:46:48 -0500

    Ajustes

--- Assets/Scripts/TrackerBase_Module.cs
+++ Assets/Scripts/TrackerBase_Module.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTracker : MonoBehaviour
+public class TrackerBase_Module : MonoBehaviour
 {
     private GameObject player;
     
@@ -12,11 +12,17 @@ public class PlayerTracker : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
+    void Update()
+    {
+        Debug.Log(GetPlayer().name);
+    }
+
     public GameObject GetPlayer()
     {
         return player;
     }
 
+    //Agregue esta funcion simplemente para hacer mas facil el hecho de sacar el vector2, pero si es innecesario se borra
     public Vector2 TrackPlayer()
     {
         return player.transform.position;
