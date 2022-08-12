ccc200042c3f867bd9019743a3e45d24eb8b96b7
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-08-11 20:46:51 -0500

    Limite de usos powerups

--- Assets/Scripts test/PowerUp_Basic.cs
+++ Assets/Scripts test/PowerUp_Basic.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerUp_Basic : MonoBehaviour
 {
     [SerializeField] float strenght;
     [SerializeField] Vector2 direction;
 
+    [SerializeField] float uses = 3;
     [SerializeField] Rigidbody2D player;
+
+    [SerializeField] GameObject button;
+
     public void Activate_BasicPowerUp()
     {
         direction.Normalize();
         player.velocity = new Vector2(player.velocity.x, 0);
         player.AddForce(direction * strenght, ForceMode2D.Impulse);
+
+        uses--;
+
+        if (uses <= 0)
+        {
+            button.SetActive(false);
+        }
     }
 }
