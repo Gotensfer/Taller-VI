03e004896556703d4dc1ef664b9927e2920c9eae
jaydeeay <jarroyavep@outlook.com>
2022-08-18 21:30:04 -0500

    UI animations
    Se agregan animaciones en la UI con DOTween, modificando la posición de las interfaces y agregando animaciones pequeñas.
    
    Se hizo modificando el script de UIInterface que manej el UI Manager y allí se referencian las UI.
    
    Se hizo una copia de la escena 0.3 y luego después de terminar el proceso se eliminó esta y se le cambió el nombre a la clonada, pasando a ser 0.3

--- TallerVI Unity/Assets/Scripts/UIInterface.cs
+++ TallerVI Unity/Assets/Scripts/UIInterface.cs
@@ -2,13 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 public class UIInterface : MonoBehaviour
 {
-    [SerializeField] int Reset_To;
+    public RectTransform mainMenu, gameUI; //References for UI position
+
+    [SerializeField] int ResetTo;
+
+    //Initialiazing UI with fadeup animation
+    private void Start()
+    {
+        mainMenu.DOAnchorPos(Vector2.zero, 2);
+    }
+
     public void Reset()
     {
-        SceneManager.LoadScene(Reset_To);
+        SceneManager.LoadScene(ResetTo);
     }
     
-    
+    //Slide for UI buttons and animations (DOTween)
+    public void playUIButton()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, -1600), 0.5f);
+        gameUI.DOAnchorPos(new Vector2(0, 0), 1);
+    }
+    public void closeUIButton()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 1);
+        gameUI.DOAnchorPos(new Vector2(3000, 0), 1);
+    }
 }
