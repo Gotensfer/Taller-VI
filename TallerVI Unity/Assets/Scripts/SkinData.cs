8415eabe63c5dd8b71e793a6a8fff53269cd9519
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-10-02 18:54:58 -0500

    Avances sistema de skins

---
+++ TallerVI Unity/Assets/Scripts/SkinData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SkinID
+{
+    NotSetted = -1,
+    Base = 0,
+    Kkawai = 1
+}
+
+public static class SkinData
+{
+    public static SkinID ID = SkinID.NotSetted;
+}
---
+++ TallerVI Unity/Assets/Scripts/SkinLoad_Module.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinLoad_Module : MonoBehaviour
+{
+    [SerializeField] AnimatorOverrideController kkawaiSkinOverride;
+
+    private void Start()
+    {
+        
+    }
+}
