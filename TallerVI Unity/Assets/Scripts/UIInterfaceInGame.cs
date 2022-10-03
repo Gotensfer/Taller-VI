8ea5eda516258dee12890ac15464fc87e680f88a
jaydeeay <jarroyavep@outlook.com>
2022-10-03 13:44:03 -0500

    Update UI InGame
    Se agrega el menú de pausa, se usan botones del summary. Queda organizarlo en orden para que quede sobre el jugador Y QUE LOS BOTONES FUNCIONEN

--- TallerVI Unity/Assets/Scripts/UIInterfaceInGame.cs
+++ TallerVI Unity/Assets/Scripts/UIInterfaceInGame.cs
@@ -15,10 +15,14 @@ public class UIInterfaceInGame : MonoBehaviour
     [SerializeField] private RectTransform altitude;
     [SerializeField] private RectTransform velocity;
 
+    [SerializeField] private RectTransform Pause;
+
     public static bool alreadyInitialized = false;
 
     private void Start()
     {
+        Pause.transform.localScale = Vector3.zero;
+
         distance.DOAnchorPosX(-37,1);
         altitude.DOAnchorPosX(-1240, 1);
         velocity.DOAnchorPosX(-1240, 1);
@@ -29,14 +33,23 @@ public class UIInterfaceInGame : MonoBehaviour
     }
 
 
+    public void SetPause()
+    {        
+        Pause.DOScale(Vector3.one,0).OnComplete(()=> Time.timeScale = 0);
+    }
 
-
+    public void SetResume()
+    {
+        Time.timeScale =1;
+        Pause.DOScale(Vector3.zero, 0.5f);
+    }
 
     public void Reset(int sceneIndex)
     {
         if (sceneIndex < 0) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene(sceneIndex);
         alreadyInitialized = true;
+        Time.timeScale = 1;
     }
 
     private void OnDisable()
