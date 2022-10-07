15a8760d57690e7d8887c27a0e9bee35245cf8b9
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-10-07 11:42:09 -0500

    Funcionalidad

--- TallerVI Unity/Assets/Scripts/PowerUp_Rocket.cs
+++ TallerVI Unity/Assets/Scripts/PowerUp_Rocket.cs
@@ -5,8 +5,11 @@ public class PowerUp_Rocket : MonoBehaviour
     private TrackerBase_Module track;
     private Rigidbody2D rb;
     private bool active = false, calleable = true;
-    [SerializeField] private float time = 3.0f, force = 10f, angle = 45f;
+    [SerializeField] private float time = 3.0f, force = 10f, angle = 45f, forceLimit = 10f;
     private float conversion;
+    
+    Vector3 touchPosWorld;
+    TouchPhase touchPhase = TouchPhase.Ended;
 
     void Start()
     {
@@ -19,6 +22,7 @@ public class PowerUp_Rocket : MonoBehaviour
     void Update()
     {
         Use();
+        Touch();
     }
 
     float count = 0;
@@ -59,6 +63,29 @@ public class PowerUp_Rocket : MonoBehaviour
         }
     }
 
+    void Touch()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase)
+        {
+            touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+            Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+            
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+
+            if (hitInformation.collider != null)
+            {
+                GameObject touchedObject = hitInformation.transform.gameObject;
+
+                if(touchedObject.transform.name == "Player" && active && force<forceLimit)
+                {
+                    force += 2f;
+                    Debug.Log("Touched");
+                }
+            }
+        }
+    }
+    
     void CallEvent()
     {
         if (calleable)
