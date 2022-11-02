a151dea1ea5a19967feaecb7491f2239ecfea83e
Jose Daniel Montoya Molina <66889936+panchuel@users.noreply.github.com>
2022-11-02 15:53:32 -0500

    Cinematica y Sonido implementados y sincronizados

---
+++ TallerVI Unity/Assets/Scripts test/Sfx/PlaySoundInCinematic.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlaySoundInCinematic : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer cinematic;
+
+    private bool flag = false;
+
+    void Update()
+    {
+        if (!flag)
+        {
+            Invoke("PlayVideo", 0.05f);
+            flag = true;
+        }
+    }
+
+    void PlaySound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Cinematica/AudioCinematica");
+        //soundCinematic.Play();
+    }
+
+    void PlayVideo()
+    {
+        cinematic.Play();
+        PlaySound();
+    }
+}
